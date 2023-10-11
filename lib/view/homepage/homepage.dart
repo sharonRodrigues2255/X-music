f6bf6d7afc45c36f983ac35a131c769501a861bf
Sharon Rodrigues <sr979151@gmail.com>
2023-10-12 00:10:44 +0530

    home page songs displayed day 2

--- lib/view/homepage/homepage.dart
+++ lib/view/homepage/homepage.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:musicplayer_project/bloc/homepagebloc/homepage_bloc.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Xusic"),
-      ),
+    final homepagebloc = HomepageBloc();
+    return BlocBuilder<HomepageBloc, HomepageState>(
+      bloc: homepagebloc,
+      builder: (context, state) {
+        final songs = state.allSongs;
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Xusic"),
+            centerTitle: true,
+          ),
+          body: ListView.builder(
+              itemCount: songs.length,
+              itemBuilder: (context, index) {
+                final song = songs[index];
+                return ListTile(
+                  title: Text(song.displayName),
+                  subtitle: Text(song.artist!),
+                  leading:
+                      QueryArtworkWidget(id: song.id, type: ArtworkType.AUDIO),
+                  trailing: Icon(Icons.more_vert),
+                );
+              }),
+        );
+      },
     );
   }
 }
