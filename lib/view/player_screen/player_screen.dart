bca5321ab997cbb80a0847b7568431eea26c1357
Sharon Rodrigues <sr979151@gmail.com>
2023-10-13 23:56:15 +0530

    next and prvious done day 4

--- lib/view/player_screen/player_screen.dart
+++ lib/view/player_screen/player_screen.dart
@@ -3,21 +3,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:musicplayer_project/bloc/player/player_bloc.dart';
 import 'package:musicplayer_project/model/mysongmodel.dart';
 import 'package:musicplayer_project/utils/constants/sizes.dart';
+import 'package:musicplayer_project/view/player_screen/widgets/player_action_row.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 class PlayerScreen extends StatelessWidget {
-  const PlayerScreen({super.key, required this.mysongs});
+  const PlayerScreen({Key? key, required this.mysongs}) : super(key: key);
+
   final List<MySongModel> mysongs;
 
   @override
   Widget build(BuildContext context) {
     final mediaSize = MediaQuery.sizeOf(context);
     final playerBloc = BlocProvider.of<PlayerBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         leading: IconButton(
-            onPressed: () {}, icon: Icon(Icons.keyboard_arrow_down_outlined)),
+          onPressed: () {},
+          icon: Icon(Icons.keyboard_arrow_down_outlined),
+        ),
       ),
       body: Center(
         child: BlocBuilder<PlayerBloc, PlayerState>(
@@ -26,42 +31,19 @@ class PlayerScreen extends StatelessWidget {
             return Column(
               children: [
                 Container(
-                  child: QueryArtworkWidget(
-                      id: mysongs[state.index!].id, type: ArtworkType.AUDIO),
                   width: mediaSize.width * .8,
                   height: mediaSize.width * .8,
+                  child: QueryArtworkWidget(
+                    id: mysongs[state.index!].id,
+                    type: ArtworkType.AUDIO,
+                  ),
                 ),
                 kheight20,
                 kwidth20,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton.filled(
-                      onPressed: () {},
-                      icon: Icon(Icons.skip_previous),
-                      iconSize: 50,
-                    ),
-                    IconButton.filled(
-                      onPressed: () {
-                        if (state.playing == true) {
-                          BlocProvider.of<PlayerBloc>(context).add(PauseSong());
-                        } else {
-                          BlocProvider.of<PlayerBloc>(context)
-                              .add(ContinueSong());
-                        }
-                      },
-                      icon: state.playing == false
-                          ? Icon(Icons.play_circle)
-                          : Icon(Icons.pause_circle),
-                      iconSize: 50,
-                    ),
-                    IconButton.filled(
-                      onPressed: () {},
-                      icon: Icon(Icons.skip_next),
-                      iconSize: 50,
-                    ),
-                  ],
-                )
+                PlayerActionRow(
+                  state: state,
+                  songs: mysongs,
+                ),
               ],
             );
           },
---
+++ lib/view/player_screen/widgets/player_action_row.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:musicplayer_project/bloc/player/player_bloc.dart';
+import 'package:musicplayer_project/model/mysongmodel.dart';
+
+class PlayerActionRow extends StatelessWidget {
+  const PlayerActionRow({Key? key, required this.state, required this.songs})
+      : super(key: key);
+
+  final PlayerState state;
+  final List<MySongModel> songs;
+
+  @override
+  Widget build(BuildContext context) {
+    final playerBloc = BlocProvider.of<PlayerBloc>(context);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton.filled(
+          onPressed: () {
+            playerBloc.add(PlaySong(
+                index: state.index! == 0 ? songs.length - 1 : state.index! - 1,
+                mysongs: songs));
+          },
+          icon: Icon(Icons.skip_previous),
+          iconSize: 50,
+        ),
+        IconButton.filled(
+          onPressed: () {
+            if (state.playing == true) {
+              playerBloc.add(PauseSong());
+            } else {
+              playerBloc.add(ContinueSong());
+            }
+          },
+          icon: state.playing == false
+              ? Icon(Icons.play_circle)
+              : Icon(Icons.pause_circle),
+          iconSize: 50,
+        ),
+        IconButton.filled(
+          onPressed: () {
+            print(songs.length);
+            print(state.index);
+            playerBloc.add(PlaySong(
+                index: state.index! < songs.length - 1 ? state.index! + 1 : 0,
+                mysongs: songs));
+          },
+          icon: Icon(Icons.skip_next),
+          iconSize: 50,
+        ),
+      ],
+    );
+  }
+}
