201dfe0f629c81cc1a857d74843c99216d9ff6b8
Sharon Rodrigues <sr979151@gmail.com>
2023-10-15 21:42:56 +0530

    Player and mini screen transitions finished

--- lib/view/homepage/widgets/mini_player.dart
+++ lib/view/homepage/widgets/mini_player.dart
@@ -21,91 +21,99 @@ class MIniPlayer extends StatelessWidget {
       buildWhen: (previous, current) => current.index != previous.index,
       builder: (context, state) {
         if (state.miniOn == true) {
-          return Card(
-            elevation: 10,
-            child: Container(
-              color: kblack,
-              height: 85,
-              width: double.infinity,
-              child: Column(
-                children: [
-                  BlocBuilder<PlayerBloc, PlayerState>(
-                    builder: (context, state) {
-                      return Hero(
-                        tag: "progress",
-                        child: ProgressBarWidget(
-                            onmini: true,
-                            playerBloc: playerBloc,
-                            mysongs: mysongs),
-                      );
-                    },
-                  ),
-                  kheight10,
-                  Row(children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) =>
-                                PlayerScreen(mysongs: mysongs)));
+          return GestureDetector(
+            onVerticalDragUpdate: (details) {
+              if (details.primaryDelta! < 0) {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => PlayerScreen(mysongs: mysongs)));
+              }
+            },
+            child: Card(
+              elevation: 10,
+              child: Container(
+                color: kblack,
+                height: 85,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    BlocBuilder<PlayerBloc, PlayerState>(
+                      builder: (context, state) {
+                        return Hero(
+                          tag: "progress",
+                          child: ProgressBarWidget(
+                              onmini: true,
+                              playerBloc: playerBloc,
+                              mysongs: mysongs),
+                        );
                       },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Hero(
-                            tag: 'player',
-                            child: SizedBox(
-                              child: QueryArtworkWidget(
-                                  artworkBorder: BorderRadius.circular(5),
-                                  artworkFit: BoxFit.cover,
-                                  id: mysongs[state.index!].id,
-                                  type: ArtworkType.AUDIO),
+                    ),
+                    kheight10,
+                    Row(children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  PlayerScreen(mysongs: mysongs)));
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Hero(
+                              tag: 'player',
+                              child: SizedBox(
+                                child: QueryArtworkWidget(
+                                    artworkBorder: BorderRadius.circular(5),
+                                    artworkFit: BoxFit.cover,
+                                    id: mysongs[state.index!].id,
+                                    type: ArtworkType.AUDIO),
+                              ),
                             ),
-                          ),
-                          Hero(
-                            tag: "name",
-                            child: Container(
-                              width: MediaQuery.sizeOf(context).width / 2.7,
-                              child: SingleChildScrollView(
-                                child: Center(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 5),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          mysongs[state.index!].displayName,
-                                          style: myfontBold(size: 14.0),
-                                        ),
-                                        Text(
-                                          mysongs[state.index!].artist,
-                                          style: myfontNormal(size: 12.0),
-                                        )
-                                      ],
+                            Hero(
+                              tag: "name",
+                              child: Container(
+                                width: MediaQuery.sizeOf(context).width / 2.7,
+                                child: SingleChildScrollView(
+                                  child: Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 5),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            mysongs[state.index!].displayName,
+                                            style: myfontBold(size: 14.0),
+                                          ),
+                                          Text(
+                                            mysongs[state.index!].artist,
+                                            style: myfontNormal(size: 12.0),
+                                          )
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
                             ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Hero(
+                            tag: "action",
+                            child: PlayerActionRow(
+                              state: state,
+                              songs: mysongs,
+                              size: 35,
+                            ),
                           ),
                         ],
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Hero(
-                          tag: "action",
-                          child: PlayerActionRow(
-                            state: state,
-                            songs: mysongs,
-                            size: 35,
-                          ),
-                        ),
-                      ],
-                    )
-                  ]),
-                ],
+                      )
+                    ]),
+                  ],
+                ),
               ),
             ),
           );
--- lib/view/player_screen/player_screen.dart
+++ lib/view/player_screen/player_screen.dart
@@ -18,119 +18,129 @@ class PlayerScreen extends StatelessWidget {
     final mediaSize = MediaQuery.sizeOf(context);
     final playerBloc = BlocProvider.of<PlayerBloc>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: Icon(Icons.keyboard_arrow_down_outlined),
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        if (details.primaryDelta! > 0) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.keyboard_arrow_down_outlined),
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            BlocBuilder<PlayerBloc, PlayerState>(
-              buildWhen: (previous, current) {
-                return previous.index != current.index;
-              },
-              builder: (context, state) {
-                return Hero(
-                  tag: "player",
-                  child: Container(
-                    width: mediaSize.width * .8,
-                    height: mediaSize.width * .8,
-                    child: QueryArtworkWidget(
-                      id: mysongs[state.index!].id,
-                      type: ArtworkType.AUDIO,
-                    ),
-                  ),
-                );
-              },
-            ),
-            BlocBuilder<PlayerBloc, PlayerState>(
-              buildWhen: (previous, current) {
-                return previous.index != current.index;
-              },
-              builder: (context, state) {
-                return Hero(
-                  tag: "name",
-                  child: Text(
-                    mysongs[state.index!].title,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                );
-              },
-            ),
-            BlocBuilder<PlayerBloc, PlayerState>(
-              buildWhen: (previous, current) {
-                return previous.index != current.index;
-              },
-              builder: (context, state) {
-                return Text(mysongs[state.index!].artist);
-              },
-            ),
-            BlocBuilder<PlayerBloc, PlayerState>(
-              builder: (context, state) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.favorite_border)),
-                    IconButton(
-                        onPressed: () {
-                          playerBloc.add(LoopAndShuffle(
-                              loop: !state.loop,
-                              shuffle:
-                                  state.loop == false ? false : state.shuffle));
-                        },
-                        icon: Icon(
-                          Icons.loop_sharp,
-                          color: state.loop ? kred : kwhite,
-                        )),
-                    IconButton(
-                        onPressed: () {
-                          playerBloc.add(LoopAndShuffle(
-                              loop: state.shuffle == false ? false : state.loop,
-                              shuffle: !state.shuffle));
-                        },
-                        icon: Icon(
-                          Icons.shuffle,
-                          color: state.shuffle ? kred : kwhite,
-                        )),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.playlist_add)),
-                  ],
-                );
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: BlocBuilder<PlayerBloc, PlayerState>(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              BlocBuilder<PlayerBloc, PlayerState>(
+                buildWhen: (previous, current) {
+                  return previous.index != current.index;
+                },
                 builder: (context, state) {
                   return Hero(
-                    tag: "progress",
-                    child: ProgressBarWidget(
-                        onmini: false,
-                        playerBloc: playerBloc,
-                        mysongs: mysongs),
+                    tag: "player",
+                    child: Container(
+                      width: mediaSize.width * .8,
+                      height: mediaSize.width * .8,
+                      child: QueryArtworkWidget(
+                        id: mysongs[state.index!].id,
+                        type: ArtworkType.AUDIO,
+                      ),
+                    ),
                   );
                 },
               ),
-            ),
-            BlocBuilder<PlayerBloc, PlayerState>(
-              builder: (context, state) {
-                return PlayerActionRow(
-                  state: state,
-                  songs: mysongs,
-                );
-              },
-            ),
-            kheight20,
-            kwidth10
-          ],
+              BlocBuilder<PlayerBloc, PlayerState>(
+                buildWhen: (previous, current) {
+                  return previous.index != current.index;
+                },
+                builder: (context, state) {
+                  return Hero(
+                    tag: "name",
+                    child: Text(
+                      mysongs[state.index!].title,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  );
+                },
+              ),
+              BlocBuilder<PlayerBloc, PlayerState>(
+                buildWhen: (previous, current) {
+                  return previous.index != current.index;
+                },
+                builder: (context, state) {
+                  return Text(mysongs[state.index!].artist);
+                },
+              ),
+              BlocBuilder<PlayerBloc, PlayerState>(
+                builder: (context, state) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                          onPressed: () {}, icon: Icon(Icons.favorite_border)),
+                      IconButton(
+                          onPressed: () {
+                            playerBloc.add(LoopAndShuffle(
+                                loop: !state.loop,
+                                shuffle: state.loop == false
+                                    ? false
+                                    : state.shuffle));
+                          },
+                          icon: Icon(
+                            Icons.loop_sharp,
+                            color: state.loop ? kred : kwhite,
+                          )),
+                      IconButton(
+                          onPressed: () {
+                            playerBloc.add(LoopAndShuffle(
+                                loop:
+                                    state.shuffle == false ? false : state.loop,
+                                shuffle: !state.shuffle));
+                          },
+                          icon: Icon(
+                            Icons.shuffle,
+                            color: state.shuffle ? kred : kwhite,
+                          )),
+                      IconButton(
+                          onPressed: () {}, icon: Icon(Icons.playlist_add)),
+                    ],
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: BlocBuilder<PlayerBloc, PlayerState>(
+                  builder: (context, state) {
+                    return Hero(
+                      tag: "progress",
+                      child: ProgressBarWidget(
+                          onmini: false,
+                          playerBloc: playerBloc,
+                          mysongs: mysongs),
+                    );
+                  },
+                ),
+              ),
+              BlocBuilder<PlayerBloc, PlayerState>(
+                builder: (context, state) {
+                  return PlayerActionRow(
+                    state: state,
+                    songs: mysongs,
+                  );
+                },
+              ),
+              kheight20,
+              kwidth10
+            ],
+          ),
         ),
       ),
     );
