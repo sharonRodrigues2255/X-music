b15ab914e39479aa61f7f3d1a7f83a95801ca262
Sharon Rodrigues <sr979151@gmail.com>
2023-10-16 10:15:28 +0530

    All songs carousal on homepage done

--- lib/view/home_page/home_page.dart
+++ lib/view/home_page/home_page.dart
@@ -1,19 +1,77 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:musicplayer_project/bloc/all_songs/all_songs_bloc.dart';
 import 'package:musicplayer_project/utils/constants/colors.dart';
+import 'package:musicplayer_project/utils/constants/sizes.dart';
 import 'package:musicplayer_project/utils/constants/text_styles.dart';
+import 'package:musicplayer_project/view/bottom_navigation/bottom_navigation.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final allsongsBloc = AllSongsBloc(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kblack,
         title: Text(
           greeting().toString(),
-          style: myfontBold(),
+          style: myfontBold(size: 28.0),
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              "All Songs",
+              style: myfontBold(color: kred, size: 22.0),
+            ),
+          ),
+          BlocBuilder<AllSongsBloc, AllSongsState>(
+            bloc: allsongsBloc,
+            builder: (context, state) {
+              return InkWell(
+                onTap: () => selectedIndex.value = 1,
+                child: CarouselSlider(
+                    items: List.generate(
+                        state.mySongs.length,
+                        (index) => Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Container(
+                                child: ListView(
+                                  children: [
+                                    Container(
+                                      width: 300,
+                                      height: 190,
+                                      child: QueryArtworkWidget(
+                                          id: state.mySongs[index].id,
+                                          type: ArtworkType.AUDIO),
+                                    ),
+                                    kheight10,
+                                    Text(
+                                      state.mySongs[index].displayName,
+                                      style: myfontBold(),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )),
+                    options: CarouselOptions(
+                        enlargeCenterPage: true,
+                        height: 250,
+                        aspectRatio: 16 / 9,
+                        autoPlayInterval: Duration(seconds: 3),
+                        autoPlay: true,
+                        autoPlayCurve: Curves.ease)),
+              );
+            },
+          )
+        ],
       ),
     );
   }
@@ -22,12 +80,12 @@ class HomePage extends StatelessWidget {
 String greeting() {
   final time = DateTime.now().hour;
   if (time >= 23 || time <= 6) {
-    return "Good Night!";
+    return "Good Night!!";
   } else if (time >= 6 || time <= 12) {
-    return "Good Morning!";
+    return "Good Morning!!";
   } else if (time >= 12 || time <= 17) {
-    return "Good Afternoon!";
+    return "Good Afternoon!!";
   } else {
-    return "Good Evening!";
+    return "Good Evening!!";
   }
 }
