4e253b6cfb151571ccdb558fc40600385ca0c520
Sharon Rodrigues <sr979151@gmail.com>
2023-11-22 22:33:00 +0530

    updates

--- android/app/src/main/kotlin/com/sharonrodrigues/xmusic/MainActivity.kt
+++ android/app/src/main/kotlin/com/sharonrodrigues/xmusic/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.musicplayer_project
+package com.sharonrodrigues.xmusic
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // Your additional code goes here
 }
